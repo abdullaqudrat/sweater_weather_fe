e2f86b4eac109d79f00fe42ccc98337baed198c8
abdullaqudrat <aqudrat630@gmail.com>
2019-05-02 20:27:55 -0600

    Edits webpack and babelrc
    -removes unused code
    - edits babelrc env to correct dev server error

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,21 @@ const path = require('path');
 module.exports = {
   entry: {
     main: "./lib/index.js",
-    test: "mocha!./test/index.js"
   },
   output: {
     path: __dirname,
     filename: "[name].bundle.js"
   },
+  mode: 'development',
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
-      { test: /\.(png|svg|jpg|gif)$/, loaders: "file-loader" },
-    ]
+   rules: [
+     { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+     { test: /\.css$/, loader: "style!css" },
+     { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+   ]
   },
   resolve: {
-    extensions: ['', '.js', '.json', '.css', '.scss']
+    extensions: ['.js', '.json', '.css']
   }
+
 };
